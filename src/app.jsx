b14449e1a7ba5def57f8af90013b7022bb2dd958
Shobika187 <psho1807@gmail.com>
2024-07-19 15:50:34 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Stopwatch from './Component/stopwatch';
+import './app.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
